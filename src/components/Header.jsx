9e021c34425c26fedce917bd1b1bc7acb8d10b35
Victor Apaez <vic.apaez@gmail.com>
2022-10-27 13:55:40 -0400

    added icons to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,28 +5,59 @@ function Header() {
     <HeaderContainer>
       <Left>
         <Logo>NETFLIX</Logo>
+        <Links>
+          <Link>Home</Link>
+          <Link>TV Shows</Link>
+          <Link>Movies</Link>
+          <Link>New & Popular</Link>
+        </Links>
       </Left>
 
       <Right>
-        <h2>ICON</h2>
+        <span class="material-symbols-outlined">search</span>
+        <span class="material-symbols-outlined">notifications</span>
+        <span class="material-symbols-outlined">expand_more</span>
       </Right>
     </HeaderContainer>
   );
 }
-
 export default Header;
 
 const HeaderContainer = styled.header`
-  height: 60px;
+  position: sticky;
+  height: 80px;
+  top: 0;
   display: flex;
   justify-content: space-between;
   padding: 0 30px;
+  background-color: black;
+  z-index: 999;
 `;
 
-const Left = styled.div``;
-
-const Logo = styled.h3`
-  color: red;
+const Left = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
-const Right = styled.div``;
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  font-size: 0.8rem;
+`;
+const Link = styled.p`
+  margin: 0 10px;
+`;
+
+const Logo = styled.h2`
+  color: #e50915;
+`;
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > * {
+    margin: 0 10px;
+  }
+`;
