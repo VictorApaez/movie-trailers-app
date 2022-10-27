96d8146cbba9a1ea8bcac4384c1ed152d2e58e6a
Victor Apaez <vic.apaez@gmail.com>
2022-10-27 12:08:38 -0400

    created movie trailer

---
+++ src/components/Trailer.jsx
@@ -0,0 +1,16 @@
+function Trailer() {
+  return (
+    <iframe
+      width="100%"
+      margin-top="-80px"
+      height="500px"
+      src="https://www.youtube.com/embed/i_mAWKyfj6c?rel=0&controls=0&modestbranding=1&autoplay=1&loop=1"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  );
+}
+
+export default Trailer;
