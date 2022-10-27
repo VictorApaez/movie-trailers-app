b890d6d7adb5a8cff29ce830798c824c1bc9feb2
Victor Apaez <vic.apaez@gmail.com>
2022-10-27 13:56:49 -0400

    added hover effect to slider

--- src/components/MovieSlide.jsx
+++ src/components/MovieSlide.jsx
@@ -2,20 +2,49 @@ import styled from "styled-components";
 
 function MovieSlide(props) {
   return (
-    <Image
-      src={`https://image.tmdb.org/t/p/w185/${props.data.poster_path}`}
-    ></Image>
+    <Container>
+      <Image
+        src={`https://image.tmdb.org/t/p/w185/${props.data.poster_path}`}
+      ></Image>
+    </Container>
   );
 }
 
 export default MovieSlide;
 
-const Image = styled.div`
+const Container = styled.div`
   flex: 0 0 25%;
+  margin: 0.2rem;
+  border-radius: 2px;
+  position: relative;
+  aspect-ratio: 16/9;
+  overflow: visible;
+  transition: transform 0.5s ease-in-out;
+  cursor: pointer;
+
+  &:hover ~ div {
+    transform: translateX(36%) !important;
+  }
+  &:hover {
+    transform: translateX(0) !important;
+  }
+`;
+
+const Image = styled.div`
   background: url(${(props) => props.src});
   background-repeat: no-repeat;
   background-size: cover;
   background-clip: content-box;
-  aspect-ratio: 16/9;
-  padding: 0.5rem;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
+  transition: transform 0.5s ease-in-out;
+
+  &:hover {
+    transform: scale(1.5) !important;
+    /* transform: translateX(0) !important; */
+    z-index: 1;
+  }
 `;
