c928d67707c6b06e50b6ceea60b5cf312b2a6754
Victor Apaez <vic.apaez@gmail.com>
2022-10-27 13:58:23 -0400

    added trailer to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import styled from "styled-components";
 import Sliders from "./components/Sliders";
 import Header from "./components/Header";
+import Trailer from "./components/Trailer";
+
 function App() {
   return (
     <Container>
       <GlobalStyle />
       <Header />
+      <Trailer />
       <Sliders />
     </Container>
   );
@@ -15,7 +18,7 @@ function App() {
 export default App;
 
 const Container = styled.div`
-  color: white;
+  position: relative;
 `;
 
 const GlobalStyle = createGlobalStyle`
