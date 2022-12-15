89149fec9a57f108f13690c6bb824bf72e6a6b98
Victor Apaez <vic.apaez@gmail.com>
2022-12-15 08:21:42 -0500

    fixed margin for img poster and

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -48,19 +48,24 @@ function MoreInfo() {
   return (
     <Container>
       {(youtubeKey && (
-        <iframe
+        <Video
           width="100%"
           height="500px"
           src={`https://www.youtube.com/embed/${youtubeKey}?autoplay=1&&modestbranding=1`}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-        ></iframe>
+        ></Video>
       )) || (
-        <img
-          alt="poster"
-          src={`https://image.tmdb.org/t/p/w185/${data.poster_path}`}
-        ></img>
+        <ImgContainer>
+          <Unavailable>
+            Sorry, the trailer is currently unavailable.
+          </Unavailable>
+          <img
+            alt="poster"
+            src={`https://image.tmdb.org/t/p/w185/${data.poster_path}`}
+          ></img>
+        </ImgContainer>
       )}
       <ContainerInfo>
         <h2>{data.title || data.name}</h2>
@@ -75,13 +80,16 @@ function MoreInfo() {
 export default MoreInfo;
 
 const Container = styled.div`
-  margin-top: -90px;
   display: flex;
   flex-direction: column;
   align-items: center;
   & iframe {
     border: none;
   }
+`;
+
+const Video = styled.iframe`
+  margin-top: -80px;
 `;
 
 const ContainerInfo = styled.div`
@@ -95,3 +103,8 @@ const ContainerInfo = styled.div`
     padding: 0;
   }
 `;
+const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Unavailable = styled.p``;
